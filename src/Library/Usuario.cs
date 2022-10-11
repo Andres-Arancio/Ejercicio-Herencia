31211b3daddb423e783ba246334a674bb2bf8482
Andres-Arancio <aarancio97@gmail.com>
2022-10-10 21:01:52 -0300

    Resto del final commit

--- src/Library/Usuario.cs
+++ src/Library/Usuario.cs
@@ -18,11 +18,41 @@ namespace Herencia
             this.Nombre = newNombre;
             this.Apellido = newApellido;
             this.CI = newCI;
-            this.Foto = newFoto;
+            this.Foto = ValidarFoto(newFoto);
         }
         public void ChangeBio(string newBio)
         {
             this.Bio = newBio;
+        }
+
+        /// <summary>
+        /// Retorna la foto del usuario si esta es valida (tiene una cara sonriendo), sino devuelve null
+        /// </summary>
+        /// <param name="newFoto"></param>
+        /// <returns></returns>
+        private static string ValidarFoto(string newFoto)
+        {
+            CognitiveCoreUCU.CognitiveFace cog = new CognitiveCoreUCU.CognitiveFace(true);
+            string defaultImage = "C:/Users/aaran/OneDrive/Documentos/Programacion II/Programas/Repositorios/Ejercicio-Herencia/asset/foto_04_exceptionimage.png";
+            
+            if(newFoto.Equals(""))
+            {
+                Console.WriteLine("No picture found. Providing default profile picture");
+                return defaultImage;
+            }
+            
+            cog.Recognize(newFoto);
+
+            if(!cog.FaceFound || !cog.SmileFound)
+            {
+                Console.WriteLine("Face not valid. Providing default profile picture");
+                return defaultImage;
+            }
+            else
+            {
+                Console.WriteLine("Face recognized and validated");
+                return newFoto;
+            }
         }
 
     }
